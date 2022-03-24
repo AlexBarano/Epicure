eb6fb8f32c7bfed1fe8ac4f8566c4a18f22a7a70
Alex Baranov <alexb@moveo.co.il>
2022-03-24 17:25:55 +0200

    adding 33% of the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,10 +8,18 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CardComponent } from './components/card/card.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomePageComponent, CardComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    CardComponent,
+    FooterComponent,
+    InputComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IDish } from 'src/app/models/dish.model';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  dishes: IDish[] = [];
+  constructor() {
+    const dish1: IDish = {
+      id: 0,
+      name: 'Claro',
+      chefName: 'Ran Shmueli',
+      img: '/assets/claro.png',
+    };
+    const dish2: IDish = {
+      id: 0,
+      name: 'Lumina',
+      chefName: 'Meir Adoni',
+      img: '/assets/mizlala-gret-mullet-fillet.png',
+    };
+    const dish3: IDish = {
+      id: 0,
+      name: 'Tiger Lilly',
+      chefName: 'Yanir Green',
+      img: '/assets/tiger-lili.png',
+    };
+    this.dishes.push(dish1);
+    this.dishes.push(dish2);
+    this.dishes.push(dish3);
   }
 
+  ngOnInit(): void {}
 }
