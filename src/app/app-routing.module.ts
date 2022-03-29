6710659a219b88bc22c188bd59319a98bb78b127
Alex Baranov <alexb@moveo.co.il>
2022-03-29 09:25:20 +0300

    Adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChefsPageComponent } from './pages/chefs-page/chefs-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RestaurantsPageComponent } from './pages/restaurants-page/restaurants-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'chefs', component: ChefsPageComponent },
+  { path: 'restaurants', component: RestaurantsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
